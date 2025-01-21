77ca164183cc9e84d3184bfa7110896e3cf7ef22
Khumoyun <humoyunortiqov05@gmail.com>
2025-01-21 21:50:40 +0500

    all task completed except

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('detail/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='detail'),
-    path('search_results/', views.post_search, name='search_results'),
     path('filter_results/', views.filter_results, name='filter_results'),
     path('post_comments/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_comment, name='post_comments'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,11 +3,14 @@ from catalogs.models import Catalog
 from tags.models import Tag
 from .models import Post, Comment
 
-
 def home(request):
-    posts = Post.objects.all()
     catalogs = Catalog.objects.all()
     tags = Tag.objects.all()
+    search = request.GET.get('search')
+    if search:
+        posts = Post.objects.filter(title__icontains=search)
+    else:
+        posts = Post.objects.all()
     ctx = {'posts': posts, 'catalogs': catalogs, 'tags': tags}
     return render(request, 'index.html', ctx)
 
@@ -28,37 +31,25 @@ def filter_results(request):
     end_date = request.GET.get('end_date')
     hashtags = request.GET.getlist('hashtag')
 
-    results = []
+    posts = []
     if category or sort_by or start_date or end_date or hashtags:
-        results = Post.objects.all()
+        posts = Post.objects.all()
 
         if category:
-            results = results.filter(catalog__in=category)
+            posts = posts.filter(catalog__in=category)
         if sort_by:
             if sort_by == 'latest':
-                results = results.order_by('-created_at')
+                posts = posts.order_by('-created_at')
             elif sort_by == 'oldest':
-                results = results.order_by('created_at')
+                posts = posts.order_by('created_at')
         if start_date and end_date:
-            results = results.filter(created_at__range=[start_date, end_date])
+            posts = posts.filter(created_at__range=[start_date, end_date])
         if hashtags:
-            results = results.filter(tags__id__in=hashtags).distinct()
+            posts = posts.filter(tags__id__in=hashtags).distinct()
     catalogs = Catalog.objects.all()
     tags = Tag.objects.all()
-    ctx = {'results': results, 'catalogs': catalogs, 'tags': tags}
-    return render(request, 'post/filter_results.html', ctx)
-
-def post_search(request):
-    catalogs = Catalog.objects.all()
-    tags = Tag.objects.all()
-    search = request.GET.get('search')
-    if search:
-        results = Post.objects.filter(title__icontains=search)
-    else:
-        results = Post.objects.none()
-    ctx = {'results': results, 'catalogs': catalogs, 'tags': tags}
-    return render(request, 'post/search_results.html', ctx)
-
+    ctx = {'posts': posts, 'catalogs': catalogs, 'tags': tags}
+    return render(request, 'index.html', ctx)
 
 def post_comment(request, year, month, day, slug):
     post = get_object_or_404(Post,
